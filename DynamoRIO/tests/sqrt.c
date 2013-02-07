3f128a0a3ba9d32b3b17deb9a13f9ff7e12fd4cc
za-lab <ZhanarAralbayeva@gmail.com>
2013-02-07 13:27:10 +0000

    simple callgrind file

--- DynamoRIO/tests/sqrt.c
+++ DynamoRIO/tests/sqrt.c
@@ -4,7 +4,11 @@
 
 #include <math.h>
 
- 
+ int substr(float d){
+	int i = 0;
+for(i = 0; i < 100; i++)
+		d--;
+}
 
 int main(void)
 
@@ -23,10 +27,11 @@ for(z = 0; z < 10; z++)
 
       // using functions from standard library, math.h
 
+			substr(100.12);
 			double d = 2.55;
 			double k = 2.0;
 double c;
-	for(z = 0; z < 1000; z++)
+	for(z = 0; z < 10; z++)
 			c += d*d;
       printf("2 raised to the power of 3 is %f\n", pow(2, 3.3));
 return 0;
